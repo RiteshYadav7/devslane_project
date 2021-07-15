90eb1a4e9ede6ebe84d4aadd247ffbf0ed836acd
Ritesh Yadav <yadavritesh2899@gmailcom>
2021-07-15 19:51:07 +0530

    added hero logo at login page

--- src/components/AuthHero.tsx
+++ src/components/AuthHero.tsx
@@ -6,9 +6,9 @@ interface Props {}
 const AuthHero: React.FC<Props> = (props) => {
   return (
     <div className="w-1/2 h-screen bg-black flex items-center">
-        <div className="h-1/2 w-1/2 mx-auto relative">
-        <img src={hero} className="absolute" />
-        </div>
+      <div className="h-1/2 w-1/2 mx-auto relative">
+        <img src={hero} className="absolute" alt="herologo" />
+      </div>
     </div>
   );
 };
